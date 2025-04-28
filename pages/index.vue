78b1b4474833438645e7192d19877b98e16260ff
Niels Louwes <nielslouwes45@gmail.com>
2025-04-28 16:53:34 +0200

    render recipe details page

--- pages/index.vue
+++ pages/index.vue
@@ -7,10 +7,14 @@ const recipes: Data = data;
 <template>
   <div class="flex flex-col gap-4">
     <div v-for="recipe in recipes.recipes" :key="recipe.id">
-      <div class="border-2 border-purple-500 w-80 py-4 px-4 rounded-lg">
-        <h2 class="font-bold">{{ recipe.name }}</h2>
-        <p>{{ recipe.description }}</p>
-      </div>
+      <NuxtLink :to="`/recipes/${recipe.id}`">
+        <div
+          class="border-2 border-purple-500 w-80 py-4 px-4 rounded-lg hover:border-4 cursor-pointer"
+        >
+          <h2 class="font-bold">{{ recipe.name }}</h2>
+          <p>{{ recipe.description }}</p>
+        </div>
+      </NuxtLink>
     </div>
   </div>
 </template>
---
+++ pages/recipes/[id].vue
@@ -0,0 +1,26 @@
+<script lang="ts" setup>
+import data from "~/data/data.json";
+const route = useRoute();
+
+const { id } = useRoute().params;
+console.log("data", data);
+
+const getRecipeInfo = () => {
+  const x = data.recipes.find((recipe) => recipe);
+  return x;
+};
+
+const recipe = getRecipeInfo();
+console.log("route", route);
+console.log("id", id);
+console.log("recipe", recipe);
+</script>
+
+<template>
+  <div class="flex flex-col items-center w-[50%] gap-4">
+    <h2>{{ recipe?.name }}</h2>
+    <p>{{ recipe?.description }}</p>
+    <p>{{ recipe?.ingredients }}</p>
+    <p>{{ recipe?.instructions }}</p>
+  </div>
+</template>
