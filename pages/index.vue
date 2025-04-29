a1390a80c404d5882a70534bb198eeab749177a9
Niels Louwes <nielslouwes45@gmail.com>
2025-04-29 09:49:17 +0200

    add meatball recipe and fix detail id issue

--- pages/index.vue
+++ pages/index.vue
@@ -5,11 +5,11 @@ import type { Data } from "~/types/types";
 const recipes: Data = data;
 </script>
 <template>
-  <div class="flex flex-col gap-4">
+  <div class="flex flex-col gap-4 p-4">
     <div v-for="recipe in recipes.recipes" :key="recipe.id">
-      <NuxtLink :to="`/recipes/${recipe.id}`">
+      <NuxtLink :to="`/recipes/${recipe.id}`" class="p-0">
         <div
-          class="border-2 border-purple-500 w-80 py-4 px-4 rounded-lg hover:border-4 cursor-pointer"
+          class="border-2 border-purple-500 w-[95%] md:w-96 py-4 px-4 rounded-lg hover:border-4 cursor-pointer"
         >
           <h2 class="font-bold">{{ recipe.name }}</h2>
           <p>{{ recipe.description }}</p>
--- pages/recipes/[id].vue
+++ pages/recipes/[id].vue
@@ -6,8 +6,7 @@ const { id } = useRoute().params;
 console.log("data", data);
 
 const getRecipeInfo = () => {
-  const x = data.recipes.find((recipe) => recipe);
-  return x;
+  return data.recipes.find((recipe) => recipe.id === Number(id));
 };
 
 const recipe = getRecipeInfo();
@@ -17,7 +16,7 @@ console.log("recipe", recipe);
 </script>
 
 <template>
-  <div class="flex flex-col items-center w-[50%] gap-4">
+  <div class="flex flex-col items-center w-[70%] gap-4">
     <h2>{{ recipe?.name }}</h2>
     <p>{{ recipe?.description }}</p>
     <p>{{ recipe?.ingredients }}</p>
