41785ba684e04d07cba222058c3e59535d83f2ec
Niels Louwes <nielslouwes45@gmail.com>
2025-05-09 10:09:50 +0200

    dynamic grid sizes web

--- pages/index.vue
+++ pages/index.vue
@@ -5,14 +5,22 @@ import type { Data } from "~/types/types";
 const recipes: Data = data;
 </script>
 <template>
-  <div class="flex flex-col gap-4 p-4">
-    <div v-for="recipe in recipes.recipes" :key="recipe.id">
-      <NuxtLink :to="`/recipes/${recipe.id}`" class="p-0">
+  <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 px-12 py-4">
+    <div
+      v-for="(recipe, index) in recipes.recipes"
+      :key="recipe.id"
+      :class="[
+        'transition-all duration-300',
+        index % 5 === 0 ? 'md:col-span-2 lg:col-span-2' : '',
+        index % 7 === 0 ? 'md:row-span-2' : '',
+      ]"
+    >
+      <NuxtLink :to="`/recipes/${recipe.id}`" class="block h-full">
         <div
-          class="border-2 border-purple-500 w-[95%] md:w-96 py-4 px-4 rounded-lg hover:border-4 cursor-pointer"
+          class="border-2 border-purple-500 h-full py-4 px-4 rounded-lg hover:border-4 cursor-pointer transition-all duration-300"
         >
-          <h2 class="font-bold">{{ recipe.name }}</h2>
-          <p>{{ recipe.description }}</p>
+          <h2 class="font-bold text-xl">{{ recipe.name }}</h2>
+          <p class="mt-2">{{ recipe.description }}</p>
         </div>
       </NuxtLink>
     </div>
