69fe2d75839dd5c6f7636ff769611e3fd4c3132b
Niels Louwes <nielslouwes45@gmail.com>
2025-06-11 16:51:17 +0200

    add prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,14 @@
 // @ts-check
-import withNuxt from './.nuxt/eslint.config.mjs'
+import withNuxt from "./.nuxt/eslint.config.mjs";
+import prettier from "eslint-plugin-prettier";
 
-export default withNuxt(
-  // Your custom configs here
-)
+export default withNuxt([
+  {
+    plugins: {
+      prettier,
+    },
+    rules: {
+      "prettier/prettier": "error",
+    },
+  },
+]);
--- todo.md
+++ todo.md
@@ -3,3 +3,7 @@
 3. search for a recipe
 4. Option to provide link and its gets the useful information from a website? without ads and fluff
 5. FIX PRETTIER SETUP - ADD
+6. add ingredients to shopping list
+    - add shopping list page
+    - use global state to add ingredient to list
+    - move 
