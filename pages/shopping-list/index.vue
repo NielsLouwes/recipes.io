6db58ecae52cc12c666a2d084d0f119882b6154f
Niels Louwes <nielslouwes45@gmail.com>
2025-07-24 18:00:32 +0200

    remove no sleep and render ingredients only if there are any

--- pages/shopping-list/index.vue
+++ pages/shopping-list/index.vue
@@ -6,7 +6,7 @@ const shoppingList = useState<string[]>('shopping-list', () => [])
 <template>
   <div>
     <h1 class="text-2xl mb-4">Shopping list</h1>
-    <ul class="border-2 px-4 md:px-12 py-6 rounded-lg w-full">
+    <ul v-if="shoppingList.length" class="border-2 px-4 md:px-12 py-6 rounded-lg w-full">
       <li v-for="(item, index) in shoppingList" :key="index">
         {{ item }}
       </li>
